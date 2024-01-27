0893c8756d6f03f891206137d1a316d55defba5c
BorisKGB <flot3.sky@yandex.ru>
2024-01-27 16:53:24 +0300

    define basic models

---
+++ hw/hw6/models.py
@@ -0,0 +1,34 @@
+from pydantic import BaseModel, Field, field_validator
+import re
+
+
+class User(BaseModel):
+    id: int = Field(default=0, ge=0)
+    name: str = Field(..., max_length=32)
+    surname: str = Field(..., max_length=32)
+    email: str = Field(..., max_length=128)
+    password: str = Field(..., min_length=6, max_length=64)
+
+    # https://www.slingacademy.com/article/pydantic-validate-user-email-and-password/
+    # `from pydantic import EmailStr` rejected due to external dependencies
+    @classmethod
+    @field_validator("email")
+    def check_email(cls, value):
+        # use a regex to check that the email has a valid format
+        email_regex = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        if not re.match(email_regex, value):
+            raise ValueError("Invalid email address")
+        return value
+
+
+class Product(BaseModel):
+    id: int = Field(default=0, ge=0)
+    name: str = Field(..., max_length=32)
+    description: str = Field(..., max_length=128)
+    cost: float = Field(..., gt=0)
+
+
+class Order(BaseModel):
+    id: int = Field(..., ge=0)
+    user_id: int = Field(..., ge=0)
+    product_id: int = Field(..., ge=0)
